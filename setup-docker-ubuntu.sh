325e7d1a40392d5d966659e8221f1956c85f9705
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-05-08 23:45:46 +0200

    Update setup-docker-ubuntu.sh

--- setup-docker-ubuntu.sh
+++ setup-docker-ubuntu.sh
@@ -1,10 +1,9 @@
-apt-get update && apt-get upgrade -y && apt-get install curl git apt-transport-https ca-certificates -y
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get purge lxc-docker -y
-apt-cache policy docker-engine
-apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
-apt-get update
-apt-get install docker-engine -y
-service docker start
+#!/usr/bin/env bash
+
+apt update && apt upgrade -y && apt install curl git apt-transport-https ca-certificates software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update && apt install docker-ce -y
+systemctl enable docker
+systemctl restart docker
+docker run hello-world
