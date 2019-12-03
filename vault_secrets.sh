5889b51d40dcecd9944b8e2ebe705bcf2a28579f
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-12-03 11:29:54 +0200

    Create vault_secrets.sh

---
+++ vault_secrets.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+# Description:
+# Bash Script for put/get secrets to/from Vault
+#
+# Get:
+# ./vault_secrets.sh get secret/tmp/hostname
+#
+# Put:
+# ./vault_secrets.sh put secret/tmp/hostname app.example.com
+
+# global environment values
+export VAULT_HOSTNAME=${VAULT_HOSTNAME}
+export VAULT_TOKEN=${VAULT_TOKEN}
+
+# check if variables are provided
+if [ -z ${VAULT_HOSTNAME} ] || [ -z ${APP_TOKEN} ]
+then 
+	echo "Requires VAULT_HOSTNAME and APP_TOKEN env variables"
+  exit 1
+fi
+
+# check if args is passed
+if [[ $# -eq 0 ]]
+then
+    echo ""
+    echo "Secret path and value needed"
+    echo "eg: $0 secret/tmp/hostname app.example.com"
+    echo ""
+    exit 1
+fi
+
+# functions
+put_secret(){
+  curl -s -XPOST -H "X-Vault-Token: ${VAULT_TOKEN}" -d '{"'"${SECRET_KEY}"'": "'"$SECRET_VALUE"'"}' https://${VAULT_HOSTNAME}/v1/${SECRET}
+}
+
+get_secret(){
+  curl -s -XGET -H "X-Vault-Token: ${VAULT_TOKEN}" https://${VAULT_HOSTNAME}/v1/${SECRET} | jq -r ".data.${SECRET_KEY}"
+}
+
+# main script
+if [ ${1} == "help" ]
+then
+	echo ""
+	echo "Secrets Management Help:"
+	echo "========================"
+	echo "${0} put secret/application-name/hostname app.example.com"
+	echo "${0} get secret/application-name/hostname"
+	echo ""
+	exit 0
+
+elif [ ${1} == "put" ] 
+then
+	if [[ $# -ne 3 ]]
+        then
+		echo ""
+                echo "PutSecret requires the key and value to retrieve the secret"
+                echo "usage: $0 put secret/application-name/secret-key secret-value"
+                echo ""
+                exit 1
+	fi
+
+	SECRET=${2}
+	SECRET_KEY=$(echo ${SECRET} | rev | cut -d'/' -f1 | rev)
+	SECRET_VALUE=${3}
+	put_secret ${SECRET} ${SECRET_VALUE}
+	echo "Secret: ${SECRET} has been written"
+	exit 0
+
+elif [ ${1} == "get" ]
+then
+	if [[ $# -ne 2 ]]
+	then 
+		echo ""
+                echo "GetSecret requires the key to retrieve the secret"
+                echo "usage: $0 get secret/application-name/secret-key"
+                echo ""
+                exit 1
+	fi
+	
+	SECRET=${2}
+	SECRET_KEY=$(echo ${SECRET} | rev | cut -d'/' -f1 | rev)
+	get_secret ${SECRET}
+
+else
+	echo "require: put/get as a parameter"
+	exit 1
+
+fi
