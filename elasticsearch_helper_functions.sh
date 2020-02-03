417a61b15e79f6975eb0f7a047724d71d5b56798
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-02-03 13:53:25 +0200

    Create elasticsearch_helper_functions.sh

---
+++ elasticsearch_helper_functions.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ELASTICSEARCH_ENDPOINT=""
+ELASTICSEARCH_SNAPSHOT_REPO=""
+
+print_help_in_elasticsearch_functions(){
+  echo "index_exist_in_snapshot snapshot_name index_name"
+}
+
+index_exist_in_snapshot(){
+  curl -s -XGET "${ELASTICSEARCH_ENDPOINT}/_snapshot/${ELASTICSEARCH_SNAPSHOT_REPO}/$1" | jq ".snapshots[].indices[] | select(. | contains(\"$2\"))"
+}
