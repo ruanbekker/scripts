b80a2ff9029c4f593ce7a7f8ffa4906344bee881
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-02-03 14:45:06 +0200

    Update elasticsearch_helper_functions.sh

--- elasticsearch_helper_functions.sh
+++ elasticsearch_helper_functions.sh
@@ -5,8 +5,13 @@ ELASTICSEARCH_SNAPSHOT_REPO=""
 
 print_help_in_elasticsearch_functions(){
   echo "index_exist_in_snapshot snapshot_name index_name"
+  echo "list_running_snapshots"
 }
 
 index_exist_in_snapshot(){
   curl -s -XGET "${ELASTICSEARCH_ENDPOINT}/_snapshot/${ELASTICSEARCH_SNAPSHOT_REPO}/$1" | jq ".snapshots[].indices[] | select(. | contains(\"$2\"))"
+}
+
+list_running_snapshots(){
+  curl -s -XGET "${ELASTICSEARCH_ENDPOINT}/_snapshot/index-backups/_all/_status?pretty"
 }
