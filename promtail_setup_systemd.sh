300553c25e5511b499f12886f2561cce6133aa82
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-03-23 11:02:59 +0200

    Create promtail_setup_systemd.sh

---
+++ promtail_setup_systemd.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+set -ex
+# only on debian atm
+command apt
+
+PROMTAIL_USER="grafana"
+PROMTAIL_VERSION="1.3.0"
+LOKI_URL=""
+
+create_user(){
+ useradd --no-create-home --shell /bin/false ${PROMTAIL_USER}
+}
+
+if [ $(id -u root > /dev/null 2>&1 ; echo $?) = 0 ]
+  then 
+    echo "${PROMTAIL_USER} user exists"
+  else 
+    echo "${PROMTAIL_USER} user does not exist, creating.."
+    create_user
+fi
+
+apt update && apt install unzip -y
+wget https://github.com/grafana/loki/releases/download/v${PROMTAIL_VERSION}/promtail-linux-amd64.zip
+unzip promtail-linux-amd64.zip
+mv promtail-linux-amd64 /usr/local/bin/promtail
+rm -rf promtail-linux-amd64.zip
+
+mkdir -p /var/lib/promtail /etc/promtail
+chown -R ${PROMTAIL_USER}:${PROMTAIL_USER} /var/lib/promtail /etc/promtail
+
+cat > /etc/systemd/system/promtail.service << EOF
+[Unit]
+Description=Promtail
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/promtail -config.file /etc/promtail/promtail-config.yml
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /etc/promtail/promtail-config.yml << EOF
+server:
+  http_listen_port: 9080
+  grpc_listen_port: 0
+
+positions:
+  filename: /var/lib/promtail/positions.yaml
+
+clients:
+  - url: ${LOKI_URL}/loki/api/v1/push
+
+scrape_configs:
+  - job_name: journal
+    journal:
+      max_age: 12h
+      path: /var/log/journal
+      labels:
+        job: systemd-journal
+        env: production
+        host: $HOSTNAME
+    relabel_configs:
+      - source_labels: ['__journal__systemd_unit']
+        target_label: 'unit'
+      - source_labels: ['__journal__hostname']
+        target_label: 'hostname'
+EOF
+
+systemctl daemon-reload
+systemctl start promtail
+systemctl enable promtail
