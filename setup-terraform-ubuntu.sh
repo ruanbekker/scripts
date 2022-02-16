aa818bddb3a2fdde0757cc6f912a2679a112d395
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-02-17 00:58:26 +0200

    Create setup-terraform-ubuntu.sh

---
+++ setup-terraform-ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt update && sudo apt install gnupg software-properties-common curl -y
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update && sudo apt install terraform -y
