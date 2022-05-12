a6e98af357820bd959a69c7f720a4e2d1004d7c7
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-05-12 17:58:42 +0200

    fix for loop

--- mysql_basic_anonamize_column_values.sh
+++ mysql_basic_anonamize_column_values.sh
@@ -3,8 +3,10 @@ set -e
 
 # variables
 DB_HOST=127.0.0.1
+DB_PORT=3306
 DB_USER=root
 DB_PASS=rootpassword
+
 MAIN_DB=prod_db
 MASKED_DB=staging_db
 TABLE_NAME=customers
@@ -30,28 +32,28 @@ function generate_fake_creditcard_num() {
 
 echo -e "\n:: dumping maindb ${MAIN_DB} to ${MAIN_DB}.sql\n"
 sleep 2
-mysqldump -u ${DB_USER} -p${DB_PASS} ${MAIN_DB} > ${MAIN_DB}.sql
+mysqldump -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASS} ${MAIN_DB} > ${MAIN_DB}.sql
 
 echo -e ":: creating stagingdb ${MASKED_DB} if not already exists\n"
 sleep 2
-mysql -u ${DB_USER} -p${DB_PASS} -e "CREATE DATABASE IF NOT EXISTS ${MASKED_DB};"
+mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASS} -e "CREATE DATABASE IF NOT EXISTS ${MASKED_DB};"
 
 echo -e ":: importing dumped ${MAIN_DB}.sql into ${MASKED_DB}\n"
 sleep 2
-mysql -u ${DB_USER} -p${DB_PASS} ${MASKED_DB} < ${MAIN_DB}.sql
+mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASS} ${MASKED_DB} < ${MAIN_DB}.sql
 
 echo -e ":: looping through the table ${TABLE_NAME} on ${MASKED_DB} and updating the column values of creditcard_number to dummy data\n"
 sleep 3
-mysql -u ${DB_USER} -p${DB_PASS} -e "SELECT username, creditcard_number FROM ${MASKED_DB}.${TABLE_NAME};" | while read username creditcard_number
+mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASS} -e "SELECT customer_id, creditcard_number FROM ${MASKED_DB}.${TABLE_NAME};" | while read customer_id creditcard_number
 do
   fake_creditcard_num=$(generate_fake_creditcard_num)
-  mysql -u ${DB_USER} -p${DB_PASS} ${MASKED_DB} -e "UPDATE ${MASKED_DB}.${TABLE_NAME} SET creditcard_number=\"$fake_creditcard_num\";"
+  mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASS} ${MASKED_DB} -e "UPDATE ${MASKED_DB}.${TABLE_NAME} SET creditcard_number=\"$fake_creditcard_num\" WHERE customer_id=$customer_id ;"
 done
 
 echo -e ":: reading table data from ${MASKED_DB}.${TABLE_NAME}\n"
-mysql -u ${DB_USER} -p${DB_PASS} -e "SELECT * from ${MASKED_DB}.${TABLE_NAME}"
+mysql -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASS} -e "SELECT * from ${MASKED_DB}.${TABLE_NAME}"
 sleep 1
 
 echo -e "\n:: dumping maindb ${MASKED_DB} to ${MASKED_DB}.sql\n"
 sleep 2
-mysqldump -u ${DB_USER} -p${DB_PASS} ${MASKED_DB} > ${MASKED_DB}.sql
+mysqldump -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASS} ${MASKED_DB} > ${MASKED_DB}.sql
