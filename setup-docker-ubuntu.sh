eb7f36be1ac4fd76cad67ee4b3f50f372852e5ad
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-06-07 09:22:32 +0200

    Delete setup-docker-ubuntu.sh

--- setup-docker-ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-apt update && apt upgrade -y && apt install curl git apt-transport-https ca-certificates software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update && apt install docker-ce -y
-systemctl enable docker
-systemctl restart docker
-docker run hello-world
