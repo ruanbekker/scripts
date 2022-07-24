b82bd0a504c7e67c5b5ffff1966ea0b529521758
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-07-24 11:30:27 +0200

    Create install-kubectl-linux.sh

---
+++ install-kubectl-linux.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+source <(kubectl completion bash)
+kubectl version --client
+echo "cheatsheet: https://kubernetes.io/docs/reference/kubectl/cheatsheet/"
