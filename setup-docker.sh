7fef215a71a3a71249a720d479ce8ac82bd9ab3d
Ruan Bekker <ruan.ru.bekker@gmail.com>
2022-08-22 13:40:33 +0200

    bump up to v2.10.0

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+DC_VERSION="2.10.0"
+
 curl https://get.docker.com | bash
 sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v$DC_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
