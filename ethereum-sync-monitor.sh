8313d8ba64ad053cd6d886287ba7e958cca3febb
Ruan Bekker <ruan.ru.bekker@gmail.com>
2023-05-08 18:25:04 +0200

    Update ethereum-sync-monitor.sh

--- ethereum-sync-monitor.sh
+++ ethereum-sync-monitor.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/env bash
 
 
+function getLastSyncedBlock(){
+  curl -s -XPOST -H 'Content-Type: application/json' --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":51}' http://127.0.0.1:8545 | jq -r '.result' | tr -d '\n' |  xargs -0 printf "%d"
+}
+
+function getHighestBlockNumber(){
+  curl -s -XPOST -H 'Content-Type: application/json' --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":51}' http://127.0.0.1:8545 | jq -r '.result.number' | tr -d '\n' |  xargs -0 printf "%d"
+}
+
 while true;
 do
-  current=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":51}' http://127.0.0.1:8545 | jq -r '.result.currentBlock' | tr -d '\n' |  xargs -0 printf "%d")
-  timeStamp=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["'"$current"'", false],"id":1}' localhost:8545  | jq -r ".result.timestamp" | tr -d '\n' |  xargs -0 printf "%d")
+  currentBlockNumber=$(getLastSyncedBlock)
+  highestBlockNumber=$(getHighestBlockNumber)
+  timeStamp=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["'"$currentBlockNumber"'", false],"id":1}' localhost:8545  | jq -r ".result.timestamp" | tr -d '\n' |  xargs -0 printf "%d")
   dateStamp=$(date -d @"$timeStamp")
 
-  localBlock=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":["latest", false],"id":1}' localhost:8545  | jq -r ".result" | tr -d '\n' |  xargs -0 printf "%d")
-  localBlockTimestamp=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["'"$localBlock"'", false],"id":1}' localhost:8545 | jq -r '.result.timestamp' | tr -d '\n' |  xargs -0 printf "%d")
+  highestBlockTimestamp=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["'"$highestBlockNumber"'", false],"id":1}' localhost:8545 | jq -r '.result.timestamp' | tr -d '\n' |  xargs -0 printf "%d")
   currentTime=$(date +%s)
-  seconds=$(($currentTime - $localBlockTimestamp))
+  seconds=$(($currentTime - $highestBlockTimestamp))
   hours=$(($seconds / 3600))
   minutes=$(($seconds / 60))
-
-  highest=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":51}' http://127.0.0.1:8545 | jq -r '.result.highestBlock' | tr -d '\n' |  xargs -0 printf "%d")
 
   echo ""
   echo ":: Current Date ::"
   echo "$(date)"
   echo ""
-  echo ":: Local Block Details ::"
-  echo "Blocknumber: $current"
-  echo "Current: $current"
-  echo "Highest: $highest"
-  echo "Left to sync: $(($highest - $current))"
-  echo "Blocknumber Datestamp: $dateStamp"
+  echo ":: Block Details ::"
+  echo "CurrentBlockNumber: $currentBlockNumber"
+  echo "HighestBlockNumber: $highestBlockNumber"
+  echo "Blocks Left to sync: $(($highestBlockNumber - $currentBlockNumber))"
+  echo "Current Blocknumber Datestamp: $dateStamp"
   #echo "Block: $localBlock"
   echo ""
   echo ":: Time Left ::"
